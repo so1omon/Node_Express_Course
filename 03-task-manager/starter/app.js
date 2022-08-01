3afd2d12773d320159981694fa94392a65850254
so1omon <solmin3665@gmail.com>
2022-08-01 21:15:18 +0900

    8월 1일 여기까지

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -1,1 +1,29 @@
-console.log('Task Manager App')
+
+const express=require('express');
+const app=express();
+const tasks=require('./routes/tasks')
+const connectDB=require('./db/connect')
+require('dotenv').config()
+
+//middleware
+app.use(express.static('./public')) // 8월 1일 여기까지함!!
+app.use(express.json())
+
+//routes
+
+app.use('/api/v1/tasks', tasks)
+
+const port=3000
+
+const start=async () =>{
+  try{
+    await connectDB(process.env.MONGO_URI)
+    app.listen(port, console.log('server is listening on port',port,'...'))
+
+  }catch(error){
+    console.log(error)
+  }
+}
+
+start()
+

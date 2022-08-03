05f19dcc6602333718fdb14a2053197979021795
so1omon <solmin3665@gmail.com>
2022-08-03 12:27:06 +0900

    Project 03-Task-Manager completes

--- 03-task-manager/starter/app.js
+++ 03-task-manager/starter/app.js
@@ -4,21 +4,24 @@ const app=express();
 const tasks=require('./routes/tasks')
 const connectDB=require('./db/connect')
 require('dotenv').config()
+const notFound=require('./middleware/not-found')
+const errorHandlerMiddleware=require('./middleware/error-handler')
 
 //middleware
-app.use(express.static('./public')) // 8월 1일 여기까지함!!
+app.use(express.static('./public'))
 app.use(express.json())
 
 //routes
-
 app.use('/api/v1/tasks', tasks)
+app.use(notFound)
+app.use(errorHandlerMiddleware)
 
-const port=3000
+const port=process.env.PORT || 3000
 
 const start=async () =>{
   try{
     await connectDB(process.env.MONGO_URI)
-    app.listen(port, console.log('server is listening on port',port,'...'))
+    app.listen(port, console.log(`server is listening on port ${port}...`))
 
   }catch(error){
     console.log(error)
